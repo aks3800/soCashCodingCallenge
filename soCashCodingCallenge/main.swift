66ea8174ba180057a68f359c6332e623db1674ea
Akshat Sharma <akshatsharma3800@gmail.com>
2020-02-15 11:23:59 +0530

    Initial Commit

---
+++ soCashCodingCallenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  soCashCodingCallenge
+//
+//  Created by Akshat Sharma on 15/02/20.
+//  Copyright © 2020 aks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
